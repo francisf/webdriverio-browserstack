6fcccd735a0ba532e4f4dde0f79e8936dc69ca33
francisf <francis@browserstack.com>
2019-07-02 16:23:45 +0530

    some changes to show coverage

--- tests/specs/single_test.js
+++ tests/specs/single_test.js
@@ -1,6 +1,6 @@
 describe('Google\'s Search Functionality', () => {
   it('can find search results', () => {
-      browser.url('https://whatismyipaddress.com/');
+      browser.url('https://www.whatismyip.com/');
       browser.getTitle().should.match(/what/i);
   });
 });
